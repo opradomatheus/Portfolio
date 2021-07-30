e72120b566cfefdd2a8007f9240424a449cd5a55
Matheus <tm.ferreira.prado@gmail.com>
2021-07-30 15:02:32 -0300

    main.js

--- js/main.js
+++ js/main.js
@@ -15,7 +15,21 @@ showMenu('nav-toggle','nav-menu')
 
 /*ACTIVE AND REMOVE MENU*/
 
+const navLink = document.querySelectorAll('.nav__link')
 
+function linkAction(){
+    
+    //Active link
+    navLink.forEach(n => n.classList.remove('active'))
+    this.classList.add('active')   
+
+    //Remove menu mobile
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show')
+
+}
+
+    navLink.forEach(n => n.addEventListener('click', LinkAction))
 
 /*SCROLL REVEAL ANIMATION*/
 
