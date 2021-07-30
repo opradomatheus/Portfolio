d2c112c4969799cd812618d61cb54e15cb23363c
Matheus <tm.ferreira.prado@gmail.com>
2021-07-30 14:06:11 -0300

    COMMIT

---
+++ js/main.js
@@ -0,0 +1,42 @@
+/*MENU SHOW*/
+
+const showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId)
+
+    if(toggle && nav){
+        toggle.addEventListener('click',()=>{
+            nav.classList.toggle('show')
+        })
+    }
+}
+
+showMenu('nav-toggle','nav-menu')
+
+/*ACTIVE AND REMOVE MENU*/
+
+
+
+/*SCROLL REVEAL ANIMATION*/
+
+
+
+/*SCROLL HOME/
+
+
+
+/*SCROLL ABOUT*/
+
+
+
+/*SCROLL SKILLS/
+
+
+
+/*SCROLL WORK*/
+
+
+
+
+
+/*SCROLL CONTACT*/
